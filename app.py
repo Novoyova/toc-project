6c430361b0641b2753c142f3296e0e25be8cc910
Kevin Horasia <kevinho1511@gmail.com>
2019-12-03 22:49:27 +0800

    Adding new states and transitions

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ from utils import send_text_message
 load_dotenv()
 
 machine = TocMachine(
-    states=["init", "food", "sate", "nasi_campur", "betutu", "babi_guling", "pisang_goreng"],
-    transitions=[
+    states=
+    [
+        "init", "food", "sate", "nasi_campur", "betutu", "babi_guling", "pisang_goreng",
+        "sate_detail", "nasi_detail", "betutu_detail", "babi_detail", "pisang_detail"
+    ],
+    transitions=
+    [
         {
             "trigger": "advance",
             "source": "init",
@@ -53,6 +58,12 @@ machine = TocMachine(
         },
         {
             "trigger": "advance",
+            "source": "food",
+            "dest": "food",
+            "conditions": "is_going_to_self",
+        },
+        {
+            "trigger": "advance",
             "source": "sate",
             "dest": "nasi_campur",
             "conditions": "is_next",
@@ -82,8 +93,72 @@ machine = TocMachine(
             "conditions": "is_next",
         },
         {
+            "trigger": "advance",
+            "source": "sate",
+            "dest": "sate_detail",
+            "conditions": "is_going_to_sate_detail",
+        },
+        {
+            "trigger": "advance",
+            "source": "nasi_campur",
+            "dest": "nasi_detail",
+            "conditions": "is_going_to_nasi_detail",
+        },
+        {
+            "trigger": "advance",
+            "source": "betutu",
+            "dest": "betutu_detail",
+            "conditions": "is_going_to_betutu_detail",
+        },
+        {
+            "trigger": "advance",
+            "source": "babi_guling",
+            "dest": "babi_detail",
+            "conditions": "is_going_to_babi_detail",
+        },
+        {
+            "trigger": "advance",
+            "source": "pisang_goreng",
+            "dest": "pisang_detail",
+            "conditions": "is_going_to_pisang_detail",
+        },
+        {
+            "trigger": "advance",
+            "source": "sate_detail",
+            "dest": "nasi_campur",
+            "conditions": "is_next",
+        },
+        {
+            "trigger": "advance",
+            "source": "nasi_detail",
+            "dest": "betutu",
+            "conditions": "is_next",
+        },
+        {
+            "trigger": "advance",
+            "source": "betutu_detail",
+            "dest": "babi_guling",
+            "conditions": "is_next",
+        },
+        {
+            "trigger": "advance",
+            "source": "babi_detail",
+            "dest": "pisang_goreng",
+            "conditions": "is_next",
+        },
+        {
+            "trigger": "advance",
+            "source": "pisang_detail",
+            "dest": "sate",
+            "conditions": "is_next",
+        },
+        {
             "trigger": "advance", 
-            "source": ["sate", "nasi_campur", "betutu", "babi_guling", "pisang_goreng"], 
+            "source": 
+            [
+                "sate", "nasi_campur", "betutu", "babi_guling", "pisang_goreng",
+                "sate_detail", "nasi_detail", "betutu_detail", "babi_detail", "pisang_detail"
+            ], 
             "dest": "food",
             "conditions": "is_going_to_food",
         },
@@ -94,7 +169,6 @@ machine = TocMachine(
 )
 
 app = Flask(__name__, static_url_path="")
-# machine.get_graph().draw("fsm.png", prog="dot", format="png")
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
@@ -168,8 +242,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    machine.get_graph().draw("./img/fsm.png", prog="dot", format="png")
+    return send_file("./img/fsm.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
