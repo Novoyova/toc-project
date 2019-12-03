6e14e9ad018d5adb99f0a2d37625782a4a7692e7
Kevin Horasia <kevinho1511@gmail.com>
2019-12-03 22:49:45 +0800

    Modified data

--- data.py
+++ data.py
@@ -12,56 +12,66 @@ Please let me know which food would you like to know.
     
     'sate':
     {
-        'description': '''Sate (satay) varieties:
+        'title': 'Sate (satay)',
+        'description': 'Sate are skewered and grilled meats served with spicy sauce',
+        'detail': '''Sate (or satay) are marinated, skewered and grilled meats served with spicy sauce. The meat usually consists of diced or sliced chicken, goat, mutton, beef and pork, but you'll also find satay that's made with fish, tofu, eggs or minced blends.
 
-Sate (or satay) are marinated, skewered and grilled meats served with spicy sauce. The meat usually consists of diced or sliced chicken, goat, mutton, beef and pork, but you'll also find satay that's made with fish, tofu, eggs or minced blends.
+Bali’s variant is the sate lilit. Made from either minced beef, chicken, fish, pork, or even turtle meat, it's mixed with coconut, coconut milk, and a rich blend of vegetables and spices. The satay is then wrapped around bamboo, sugarcane or lemongrass sticks before it's grilled over charcoal. You can enjoy sate lilit with or without dipping sauce.
 
-Bali’s variant is the sate lilit. Made from either minced beef, chicken, fish, pork, or even turtle meat, it's mixed with coconut, coconut milk, and a rich blend of vegetables and spices. The satay is then wrapped around bamboo, sugarcane or lemongrass sticks before it's grilled over charcoal. You can enjoy sate lilit with or without dipping sauce.''',
+(Send any message to go back to main menu)''',
 
-        'image_url': 'img/sate.jpg',
+        'image_url': 'https://i.imgur.com/Ebn3J2N.jpg',
     }, 
     
 
     'nasi_campur': 
     {
-        'description': '''Nasi ayam and nasi campur: 
+        'title': 'Nasi Ayam and Nasi Campur',
+        'description': 'White rice comes with different elements of Balinese food',
+        'detail': '''Bali’s take on chicken rice, nasi ayam and nasi campur are served at many warungs (small eateries) and restaurants throughout the island. A plate of white rice comes with different elements of Balinese food, such as a portion of babi guling (roast suckling pig) or betutu (spiced chicken or duck), mixed vegetables, and a dab of spicy sambal matah (Balinese sauce).
 
-Bali’s take on chicken rice, nasi ayam and nasi campur are served at many warungs (small eateries) and restaurants throughout the island. A plate of white rice comes with different elements of Balinese food, such as a portion of babi guling (roast suckling pig) or betutu (spiced chicken or duck), mixed vegetables, and a dab of spicy sambal matah (Balinese sauce).
+Nasi ayam and nasi campur are sometimes served with a bowl of soup. For those who do not want it too spicy, simply ask for it without the sambal.
 
-Nasi ayam and nasi campur are sometimes served with a bowl of soup. For those who do not want it too spicy, simply ask for it without the sambal.''',
+(Send any message to go back to main menu)''',
     
-        'image_url': '/img/nasi_campur.jpg',
+        'image_url': 'https://i.imgur.com/HR7DMlt.jpg',
     },
 
 
     'betutu': 
     {
-        'description': '''Bebek and ayam betutu:
+        'title': 'Bebek and Ayam Betutu',
+        'description': 'Betutu is the slow-cooked duck (bebek) or chicken (ayam)',
+        'detail': '''Betutu is the slow-cooked equivalent of Bali’s babi guling (roast suckling pig). Suitable for those who don’t eat pork, this iconic Balinese dish consists of a whole chicken (ayam) or duck (bebek) stuffed with traditional spices, wrapped in banana leaves, then enveloped tight in the bark of a banana trunk. The entire thing is baked or buried in a coal fire for 6 to 7 hours, resulting in a rich and juicy meat that easily separates from the bones.
+        
+(Send any message to go back to main menu)''',
 
-Betutu is the slow-cooked equivalent of Bali’s babi guling (roast suckling pig). Suitable for those who don’t eat pork, this iconic Balinese dish consists of a whole chicken (ayam) or duck (bebek) stuffed with traditional spices, wrapped in banana leaves, then enveloped tight in the bark of a banana trunk. The entire thing is baked or buried in a coal fire for 6 to 7 hours, resulting in a rich and juicy meat that easily separates from the bones.''',
-    
-        'image_url': './img/betutu.jpg',
+        'image_url': 'https://i.imgur.com/vDDRPFX.jpg',
     },
 
 
     'babi_guling': 
     {
-        'description': '''Babi guling:
+        'title': 'Babi Guling',
+        'description': 'Babi guling is an all-time favorite Balinese food',
+        'detail': '''Babi guling is an all-time favourite, consisting of spit-roast pig stuffed with rich traditional spices and vegetable mixes such as cassava leaves, slowly rolled over (guling means "to roll" in Indonesian) a coal fire.
 
-Babi guling is an all-time favourite, consisting of spit-roast pig stuffed with rich traditional spices and vegetable mixes such as cassava leaves, slowly rolled over (guling means "to roll" in Indonesian) a coal fire.
+The crisp brown skins are prized, while the meat is a tender and juicy treat. At first, babi guling was a communal treat during special festivities and ceremonies, but you can find it at many warungs (food stalls) and restaurants that specialise in this dish.
 
-The crisp brown skins are prized, while the meat is a tender and juicy treat. At first, babi guling was a communal treat during special festivities and ceremonies, but you can find it at many warungs (food stalls) and restaurants that specialise in this dish.''',
+(Send any message to go back to main menu)''',
     
-        'image_url' : './img/babi_guling.jpg',
+        'image_url' : 'https://i.imgur.com/WKa6suk.jpg',
     },
     
 
     'pisang_goreng': 
     {
-        'description': '''Pisang Goreng:
+        'title': 'Pisang Goreng',
+        'description': 'Fried banana served with honey, palm sugar syrup, ice cream',
+        'detail': '''Bali has such a variety of bananas from the small, sweet varieties to the large ones that look like a weapon. The different types make fried bananas a treat each time. Served with honey or palm sugar syrup, a little bit of flaked coconut or vanilla ice cream or just plain from the roadside stalls, nothing is better than Pisang Goreng.
+        
+(Send any message to go back to main menu)''',
 
-Bali has such a variety of bananas from the small, sweet varieties to the large ones that look like a weapon. The different types make fried bananas a treat each time. Served with honey or palm sugar syrup, a little bit of flaked coconut or vanilla ice cream or just plain from the roadside stalls, nothing is better than Pisang Goreng.''',
-    
-        'image_url': './img/pisang_goreng.jpg',
+        'image_url': 'https://i.imgur.com/rckT9pb.jpg',
     }
 }
