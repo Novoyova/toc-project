b1c6b1c213c7c986cb4c1f270b52048db6ce7bbf
Kevin Horasia <kevinho1511@gmail.com>
2019-12-03 09:58:41 +0800

    Fix import error

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ machine = TocMachine(
 )
 
 app = Flask(__name__, static_url_path="")
-machine.get_graph().draw("fsm.png", prog="dot", format="png")
+# machine.get_graph().draw("fsm.png", prog="dot", format="png")
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
