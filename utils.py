3c411a65486844dde419e9053a5c974509547be9
Kevin Horasia <kevinho1511@gmail.com>
2019-12-03 22:50:29 +0800

    Add send template message function

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import os
 from flask import request
-
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ButtonsTemplate
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ButtonsTemplate, PostbackAction, TemplateSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -11,38 +10,22 @@ channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
-
     return "OK"
+    
 
-
-
-def send_image_url(id, img_url):
-    url = request.url_root + img_url
-    # print("URL: ", url)
-    # url = img_url
-    # app.logger.info("url=" + url)
+def send_template_message(replay_token, title, text, image_url):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.push_message(id, ImageSendMessage(url, url))
-
+    button_template = ButtonsTemplate(
+        title=title, 
+        text=text, 
+        thumbnail_image_url=image_url,
+        actions=
+        [
+            PostbackAction(label='See details', data='any', text='Detail'),
+            PostbackAction(label='Next', data='any', text='Next'),
+            PostbackAction(label='Back to main menu', data='any', text='Back')
+        ]
+    )
+    message_template = TemplateSendMessage(alt_text='Your line is not supported' ,template=button_template)
+    line_bot_api.reply_message(replay_token, message_template)
     return "OK"
-
-
-"""
-elif text == 'buttons':
-        buttons_template = ButtonsTemplate(
-            title='My buttons sample', text='Hello, my buttons', actions=[
-                URIAction(label='Go to line.me', uri='https://line.me'),
-                PostbackAction(label='ping', data='ping'),
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-            ])
-        template_message = TemplateSendMessage(
-            alt_text='Buttons alt text', template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-"""
-
-
-"""
-def send_button_message(id, text, buttons):
-    pass
-"""
